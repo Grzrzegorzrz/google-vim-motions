02a20151c8e8612576b20c133a6232f69d9ee42e
Grzrzegorzrz <wnguye4@uwo.ca>
2025-11-02 22:51:38 -0500

    chore: init

---
+++ content.js
@@ -0,0 +1,119 @@
+const resultsContainer = document.querySelector("#search #rso");
+const searchFormRect = document
+  .querySelector("#searchform")
+  .getBoundingClientRect();
+const section = document.querySelectorAll('[aria-current="page"]');
+
+const IGNORE = ".related-question-pair, .kp-blk, .g-blk, .xpdopen, .xpd";
+const topOffset = searchFormRect.bottom - searchFormRect.top; // height of top bar
+
+let prevKeyG = false;
+let index = 0;
+
+const results = resultsContainer
+  ? [...resultsContainer.querySelectorAll("a > h3")]
+      .map(
+        (h3) =>
+          h3.parentElement.parentElement.parentElement.parentElement
+            .parentElement.parentElement,
+      )
+      .filter((a) => {
+        // Ensure it's a visible standard link and not part of special boxes
+        const parent = a.closest("[data-hveid]");
+        return parent && !parent.closest(IGNORE);
+      })
+  : [];
+
+function checkSection() {
+  const html = section[0]?.innerHTML ?? "";
+  return !(html.includes("Images") || html.includes("Short videos"));
+}
+
+function updateSelected(direction) {
+  if (!results || !results.length || !checkSection()) return;
+
+  const bottom = results.length - 1;
+
+  // remove colour from curr selection before changing selection
+  results[index].style.setProperty("background-color", "initial");
+
+  switch (direction) {
+    case "down":
+      if (index < bottom) index++;
+      break;
+    case "up":
+      if (index > 0) index--;
+      break;
+    case "first":
+      index = 0;
+      break;
+    case "last":
+      index = bottom;
+      break;
+  }
+
+  if (index == 0 || index == bottom)
+    results[index].scrollIntoView({ behavior: "auto", block: "center" });
+  else results[index].scrollIntoView({ behavior: "auto", block: "nearest" });
+
+  // move view up by amount the selected is blocked by the top bar
+  const selectedTop = results[index].getBoundingClientRect().top;
+  if (selectedTop < topOffset) window.scrollBy(0, selectedTop - topOffset);
+
+  results[index].style.setProperty("background-color", "#282828");
+  results[index].querySelector("span > a").focus();
+}
+
+updateSelected();
+
+addEventListener("keydown", (e) => {
+  const active = document.activeElement;
+  const link = results[index]?.querySelector("span > a").href;
+
+  // is typing
+  if (
+    ["TEXTAREA", "INPUT"].includes(active?.tagName) ||
+    active?.isContentEditable
+  )
+    return;
+
+  let code = e.code;
+
+  // handle trailingG
+  if (code === "KeyG") {
+    if (e.shiftKey) code = "UpG";
+    else if (prevKeyG) code = "GG";
+    else {
+      prevKeyG = true;
+      return;
+    }
+  }
+
+  prevKeyG = false;
+
+  switch (code) {
+    case "UpG":
+      updateSelected("last");
+      break;
+    case "GG":
+      updateSelected("first");
+      break;
+    case "KeyJ":
+      updateSelected("down");
+      break;
+    case "KeyK":
+      updateSelected("up");
+      break;
+
+    case "Enter":
+    case "KeyL":
+      if (!link) break;
+      e.preventDefault();
+      console.log(link);
+
+      if (e.shiftKey) window.open(link, "_blank");
+      else window.location.href = link;
+
+      break;
+  }
+});
